df41361dda1922dd666dfc1a7c9e7d1246787ebb
Salat-Tamas <tamas.salat2004@gmail.com>
2026-07-24 15:27:12 +0300

    Add spring core tranining projects

---
+++ SpringHelloWorldExample/src/main/java/spring_example/Main.java
@@ -0,0 +1,13 @@
+package spring_example;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new
+    ClassPathXmlApplicationContext("beans.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("HelloWorld");
+        obj.getMessage();
+    }
+    
+}
