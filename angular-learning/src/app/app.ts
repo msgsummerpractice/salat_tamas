3f6be3fe6d3ed3f8b7381316ec2401d00307f602
Salat-Tamas <tamas.salat2004@gmail.com>
2026-07-29 15:55:54 +0300

    Implement material design in project Use mat-toolbar Add anchor tags and use mat-button for links Give color theme Use icons

--- angular-learning/src/app/app.ts
+++ angular-learning/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule, MatButtonModule, RouterLink, MatIconModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-learning');
