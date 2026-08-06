546de5240b17d7ec81209ac8f2dbe468a261e3ec
Salat-Tamas <tamas.salat2004@gmail.com>
2026-08-06 13:56:41 +0300

    Separated template and style into separate files

--- angular-learning/src/app/components/image-viewer.component.ts
+++ angular-learning/src/app/components/image-viewer.component.ts
@@ -8,55 +8,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   selector: 'app-image-viewer',
   standalone: true,
   imports: [MatButtonModule, MatProgressSpinnerModule],
-  template: `
-    <div class="flex flex-col items-center justify-center gap-4 min-w-40">
-      <button
-        mat-raised-button
-        (click)="loadImage()"
-        [disabled]="loading()"
-        class="flex items-center gap-2"
-      >
-        @if (loading()) {
-          <mat-progress-spinner
-            mode="indeterminate"
-            diameter="18"
-            strokeWidth="3"
-            class="w-full"
-          ></mat-progress-spinner>
-        } @else {
-          <span class="w-full">Load Image</span>
-        }
-      </button>
-
-      <div class="flex flex-col items-center justify-center gap-4">
-        @if (imageUrl() && !loading()) {
-          <div>
-            <img [src]="imageUrl()" alt="Random dog image" />
-          </div>
-        }
-
-        @if (error()) {
-          <div>{{ error() }}</div>
-        }
-      </div>
-    </div>
-  `,
-  styles: [
-    `
-      img {
-        max-width: 100%;
-        height: auto;
-        max-height: 300px;
-        border-radius: 8px;
-      }
-
-      button {
-        display: inline-flex;
-        align-items: center;
-        gap: 8px;
-      }
-    `,
-  ],
+  templateUrl: './image-viewer.component.html',
+  styleUrls: ['./image-viewer.component.css'],
 })
 export class ImageViewerComponent implements OnDestroy {
   imageUrl = signal<string | null>(null);
